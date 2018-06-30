136b8df25addd4e3c9b13203c361bd88f141a48a
humangas <humangas.net@gmail.com>
2018-07-01 08:51:30 +0900

    Add permalink function

--- scripts/indexmd.sh
+++ scripts/indexmd.sh
@@ -7,6 +7,7 @@
 # $ bash scripts/indexmd.sh
 ###########################################################################
 DOCS_PATH="_docs"
+SITE_PATH="docs"
 BASE_URL="https://humangas.github.io/cheat"
 
 
@@ -21,13 +22,39 @@ contents() {
 }
 
 index() {
+    # setting required: https://squidfunk.github.io/mkdocs-material/extensions/permalinks/#installation
+
+    #$ grep '.*title="\[vim\] リロード.*' vim/index.html
+    #  <a href="#vim_2" title="[vim] リロード" class="md-nav__link">
+    #  <a href="#vim_2" title="[vim] リロード" class="md-nav__link">
+    #
+    #$ grep '.*title="\[vim\] リロード.*' vim/index.html | head -1
+    #  <a href="#vim_2" title="[vim] リロード" class="md-nav__link">
+    #
+    #$ grep '.*title="\[vim\] リロード.*' vim/index.html | head -1 | grep -Po '(?<=")[^",]+(?=")'
+    ##vim_2
+    # title=
+    #[vim] リロード
+    # class=
+    #md-nav__link
+    #
+    #$ grep '.*title="\[vim\] リロード.*' vim/index.html | head -1 | grep -Po '(?<=")[^",]+(?=")' | head -1
+    ##vim_2
+    _permalink() {
+        _tmp=$(echo "$file_path" | sed -e "s/$DOCS_PATH/$SITE_PATH/")
+        indexhtml_path="${_tmp%.*}/index.html"
+        index=$(grep ".*title=\".*$title.*\"" $indexhtml_path | head -1 | grep -Po '(?<=")[^",]+(?=")' | head -1)
+        echo "- [$title]($BASE_URL/$file/$index)"
+    }
+
     # Contents of variable $l
     # e.g. _docs/shell.md:1:## [shell] あるディレクトリ配下のファイル数をカウントする
     IFS=$'\n'
     for l in $(find $DOCS_PATH -name "*.md" -type f -print0  \
                     | xargs -0 egrep '## \[.*' \
                     | sort); do
-        _tmp=$(echo "$l" | cut -d: -f1 | sed -e "s@$DOCS_PATH/@@")
+        file_path=$(echo "$l" | cut -d: -f1)
+        _tmp=$(echo "$file_path" | sed -e "s@$DOCS_PATH/@@")
         file=$(echo ${_tmp%.*})
         
         title=$(echo "$l"  \
@@ -38,9 +65,10 @@ index() {
         if [[ $file != $header ]]; then
             echo -e "\n### $file"
         fi
-
         header=$file
-        echo "- [$title]($BASE_URL/$file)"
+
+        # echo "- [$title]($BASE_URL/$file)"
+        _permalink
     done
 }
 
