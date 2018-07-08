ca4386b18bbdd200faa68cfa24e3fa04c096bea9
humangas <humangas.net@gmail.com>
2018-07-08 22:50:44 +0900

    Add cheat command

---
+++ bin/cheat.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+APPNAME="cheat"
+CHEAT_BASE_PATH=${CHEAT_BASE_PATH:-$GOPATH/src/github.com/humangas/cheat/_docs}
+CHEAT_WEB_SITE=${CHEAT_WEB_SITE:-https://humangas.github.io/cheat/}
+
+usage() {
+echo "Usage: $APPNAME [--help] [option] [args]
+Option:
+    -f [filter]  select cheat file
+    -b           open $CHEAT_WEB_SITE
+
+Customize:
+    export CHEAT_BASE_PATH=\"$CHEAT_BASE_PATH\"
+    export CHEAT_WEB_SITE=\"$CHEAT_WEB_SITE\"
+
+Dependencies:
+    - fzf: https://github.com/junegunn/fzf
+"
+exit 0
+}
+
+item() {
+    if [[ -z $1 ]]; then
+        local res=$(find $CHEAT_BASE_PATH -type f \
+            | xargs grep -n -e '^## .*' \
+            | sed -e "s@$CHEAT_BASE_PATH/@@" -e 's/.md//' -e 's/:## /: /' \
+            | fzf \
+            --multi --cycle \
+            --bind=ctrl-u:half-page-up,ctrl-d:half-page-down \
+            )
+    else
+        local res=$(find $CHEAT_BASE_PATH -type f \
+            | xargs grep -n -e '^## .*' \
+            | sed -e "s@$CHEAT_BASE_PATH/@@" -e 's/.md//' -e 's/:## /: /' \
+            | fzf -f "$1" \
+            | fzf \
+            --multi --cycle \
+            --bind=ctrl-u:half-page-up,ctrl-d:half-page-down \
+            )
+    fi
+
+    [[ -z $res ]] && return
+    local file_name=$(echo $res | cut -d: -f1)
+    local line_num=$(echo $res | cut -d: -f2)
+    vim -c $line_num $CHEAT_BASE_PATH/$file_name.md
+}
+
+file() {
+    if [[ -z $1 ]]; then
+        local res=$(find $CHEAT_BASE_PATH -type f \
+            | sed -e "s@$CHEAT_BASE_PATH/@@" -e 's/.md//' \
+            | fzf \
+            --preview "less -R $CHEAT_BASE_PATH/{}.md" \
+            --multi --cycle \
+            --bind=ctrl-u:half-page-up,ctrl-d:half-page-down \
+            )
+    else
+        local res=$(find $CHEAT_BASE_PATH -type f \
+            | sed -e "s@$CHEAT_BASE_PATH/@@" -e 's/.md//' \
+            | fzf -f "$1" \
+            | fzf \
+            --preview "less -R $CHEAT_BASE_PATH/{}.md" \
+            --multi --cycle \
+            --bind=ctrl-u:half-page-up,ctrl-d:half-page-down \
+            )
+    fi
+
+    [[ -z $res ]] && return
+    vim "$CHEAT_BASE_PATH/$res.md"
+}
+
+browse() {
+    open $CHEAT_WEB_SITE
+}
+
+main() {
+    [[ "$1" == "--help" ]] && usage
+
+    local option="$1"
+    case $option in
+        -f)    shift && file "$@" && return ;;
+        -b)    shift && browse && return ;;
+        *)     item "$@" && return ;;
+    esac
+}
+
+main "$@"
